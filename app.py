9d027f7155c4536f9d30ea0eb9a3a05530419060
unknown <you@example.com>
2023-11-02 14:33:35 -0300

    Acessando pagina

--- app.py
+++ app.py
@@ -8,3 +8,12 @@ from selenium.webdriver.chrome.service import Service
 servico = Service(ChromeDriverManager().install())
 #usar o chrome
 navegador = webdriver.Chrome(service=servico)
+
+#Entrar no navegado
+navegador.get("https://sso.acesso.gov.br/login?client_id=5b1db4fd-87e7-4689-9c37-faa2a5663c6c&authorization_id=18b910c0020")
+
+#Encontrar o Elemento pegar xpath onde vai colocar os dados
+navegador.find_element('xpath','//*[@id="accountId"]').send_keys("12345678978")
+
+#Vai clicar no botão para entrar
+navegador.find_element('xpath','//*[@id="login-button-panel"]/button').click()
